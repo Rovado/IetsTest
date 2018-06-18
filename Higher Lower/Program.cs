82bb506af7043cdeb2a5d4766d6630879d3c9ac5
robbert.van.doorn <robbert.van.doorn@W10-RVD>
2018-06-18 17:32:13 +0200

    Studiemateriaal toegevoegd

--- Higher Lower/Program.cs
+++ Higher Lower/Program.cs
@@ -22,7 +22,13 @@ namespace Higher_Lower
             int[] numbers = { 0, 1, 2, 3, 4, 5 };
             int firstNumber = numbers[0];
             int secondNumber = numbers[1];
-            //Console.WriteLine(firstNumber, secondNumber);
+        //Console.WriteLine(firstNumber, secondNumber);
+
+        https://www.youtube.com/watch?v=0p0JLFZj2C8&list=PLGLfVvz_LVvRX6xK1oi0reKci6ignjdSa
+        https://www.youtube.com/watch?v=x_9lfHjYtVg&list=PL0EE421AE8BCEBA4A
+        https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/for
+        https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/foreach-in
+        https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/while
         }
     }
 }

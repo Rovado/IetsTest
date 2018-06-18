48761d6e5e29c7de5c8a593d51750099cee51e7a
Robbert van Doorn <robbert.van.doorn@ultimatesoftware.nl>
2018-06-18 17:26:01 +0200

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Int's zijn types. In dit geval een integer die een getal als waarde krijgt.
+            int a = 5;
+            int b = 2;
+            int c = 10;
+
+            b = a;
+            a = -3;
+            
+            
+            //$" Doet inline placeholders. Wel de values in parenthesis'!
+            Console.WriteLine($"{a}  {b}");
+
+            //{0} en {1} zijn placeholders. Index gebaseerd, begint bij 0, 1, etc.
+            Console.WriteLine("{0} {1}", a, b);
+
+            //Bewijs dat 0 en 1 placeholders zijn en gerecycled kunnen worden.
+            //De waarden hangen vast aan de gedefineerde integers.
+            Console.WriteLine("{0} {1}", c, b);
+
+            //Er is een limiet aan 32 bit en de int type is 32 bit.
+            //Als het goed is is het limiet ongeveer 2 miljard.
+            //  int OverHetLimietHeen = 4000000000;
+            //  Console.WriteLine(OverHetLimietHeen);
+
+            // MaxInt++ geeft de grooste variabele mogelijk. 
+            //In dit geval -2147483648
+
+            int maxInt = Int32.MaxValue;
+            maxInt++;
+            Console.WriteLine(maxInt);
+
+            //Er zijn 4 types 
+            {
+                long aVeryBigNumber = 395904282569;
+                aVeryBigNumber = 13;
+                Console.WriteLine(aVeryBigNumber);
+            }
+            //Hier staat een vraag voor de gebruiker; "Wat is jouw naam?"
+            Console.WriteLine("Wat is jouw naam?");
+
+            //Hier wordt ingegeven wat het antwoord is van de gebruiker.
+            string IngaveNaam = Console.ReadLine();
+            Console.WriteLine($"Okee, hallo {IngaveNaam}");
+
+            //Hier wordt een willekeurig getal aangemaakt.
+            int WillekeurigGetal = new Random().Next(100);
+
+            Console.WriteLine(WillekeurigGetal);
+
+
+        }
+    }
+}
---
+++ Higher Lower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Higher_Lower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A random number is generated here for the user to guess.
+            int RandomNumber = new Random().Next(100);
+
+            Console.WriteLine("Welcome to the game higher or lower.");
+            string[] multipleLines = { "Hello", "World" };
+            Console.WriteLine(multipleLines);
+
+
+
+            int[] numbers = { 0, 1, 2, 3, 4, 5 };
+            int firstNumber = numbers[0];
+            int secondNumber = numbers[1];
+            Console.WriteLine(firstNumber, secondNumber);
+        }
+    }
+}
